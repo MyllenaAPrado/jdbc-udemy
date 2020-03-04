1e8c0c09cf8b781f8d1d1629c5f7996751ef4e06
MyllenaAPrado <myllena.pradoa@gmail.com>
2020-03-04 08:59:39 -0300

    Create Department class and Seller class

--- src/aplication/App.java
+++ src/aplication/App.java
@@ -1,32 +1,34 @@
 package aplication;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.text.SimpleDateFormat;
+import java.util.Date;
 
-import db.DB;
-import db.DbIntegrityException;
+import entites.Department;
+import entites.Seller;
 
 public class App {
 
 	public static void main(String[] args) {
-		// DB is not instantiated because it's a service
-		Connection conn = null;
-		PreparedStatement st = null;
-		conn = DB.getConnection();
-//		try {
-//			// connect with the database
-//			conn = DB.getConnection();
-//			
-//		} catch (SQLException e) {
-//			throw new DbIntegrityException(e.getMessage());
-//		}
-		//finally{
-		// close the connection
-		DB.closeStatement(st);
-		DB.closeConnection();
-	//}
+		
+		Department department = new Department(2, "bolsas");
+		Seller seller = new Seller(21,  "bob", "bob@gmail.com", new Date(), 3000.00, department);
+		System.out.println(seller);
+
+//		// DB is not instantiated because it's a service
+//		Connection conn = null;
+//		PreparedStatement st = null;
+//		conn = DB.getConnection();
+////		try {
+////			// connect with the database
+////			conn = DB.getConnection();
+////			
+////		} catch (SQLException e) {
+////			throw new DbIntegrityException(e.getMessage());
+////		}
+//		//finally{
+//		// close the connection
+//		DB.closeStatement(st);
+//		DB.closeConnection();
+//	//}
 }
 
 }
